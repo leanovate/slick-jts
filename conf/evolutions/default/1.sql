4768dadce75aa8bf5ea3654f1cfef2a7226f902d
Rasmus Kuschel <rasmus.kuschel@immobilienscout24.de>
2015-05-20 16:49:05 +0200

    index added

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -1,7 +1,9 @@
 # --- !Ups
-CREATE TABLE `SHAPES` (
-    `id` bigint(20) NOT NULL,
-    `district_name` varchar(255) NOT NULL,
-    `shape` geometry NOT NULL,
+CREATE TABLE shapes (
+    id bigint(20) NOT NULL,
+    district_name varchar(255) NOT NULL,
+    shape geometry NOT NULL,
     PRIMARY KEY (id)
 );
+
+CREATE SPATIAL INDEX ON shapes(shape);
