0b83a003c4e5fdb271d58c419a9a41f217a8c6d8
Rasmus Kuschel <rasmus.kuschel@immobilienscout24.de>
2015-05-20 15:52:36 +0200

    findAll implementation

--- app/service/ShapeService.scala
+++ app/service/ShapeService.scala
@@ -1,6 +1,11 @@
 package service
 
+import java.sql.ResultSet
+import play.api.db.DB
 import scala.concurrent.Future
+
+import play.api.Play.current
+import scala.concurrent.ExecutionContext.Implicits.global
 
 case class BoundingBox(neLat: Double, neLng: Double, swLat: Double, swLng: Double)
 
@@ -14,9 +19,19 @@ trait ShapeService {
 
 class DemoShapeService extends ShapeService {
 
-  def listAll(): Future[List[String]] = {
-    Future.successful(List("Munnerum", "Oppau"))
+  class RsIterator(rs: ResultSet) extends Iterator[ResultSet] {
+    def hasNext: Boolean = rs.next()
+    def next(): ResultSet = rs
   }
+
+  def listAll(): Future[List[String]] =
+    Future {
+      DB.withConnection(c => {
+        val resultSet = c.prepareStatement("select district_name from shapes").executeQuery()
+        val result = new RsIterator(resultSet).map(x => x.getString(1))
+        result.toList
+      })
+    }
 
   def findByCoordinate(lat: Double, lng: Double): Future[Option[String]] = ???
   def findByBoundingBox(boundingBox: BoundingBox): Future[List[String]] = ???
