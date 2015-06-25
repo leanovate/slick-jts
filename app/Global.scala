db91d2a9fcdc195691c94491eeef6c1db44f231a
christoph <christoph.meise@leanovate.de>
2015-06-25 13:14:58 +0200

    only populate db if nothing is inside

--- app/Global.scala
+++ app/Global.scala
@@ -4,6 +4,6 @@ import play.api._
 object Global extends GlobalSettings {
 
   override def onStart(app: Application) {
-    new TestDataGenerator().populateDB()
+    new TestDataGenerator().populateDBIfNecessary()
   }
 }
--- app/database/TestDataGenerator.scala
+++ app/database/TestDataGenerator.scala
@@ -1,25 +1,32 @@
 package database
 
+import java.sql.ResultSet
+
 import jtscala.WKTHelper.WKTPreparedStatement
 import play.api.Play.current
 import play.api.db.DB
 import scala.io.Source
 
 class TestDataGenerator {
-  def populateDB(): Unit = {
+  def populateDBIfNecessary(): Unit = {
     val filename = "app/assets/testdata"
 
     val lines = Source.fromFile(filename).getLines
     DB.withConnection { implicit c =>
-      while (lines.hasNext) {
-        val (id, districtName, wkt) = (lines.next, lines.next, lines.next)
+      val valueQuery = c.prepareStatement("select count(*) from SHAPES")
+      val values: ResultSet = valueQuery.executeQuery()
 
-        val stmt = c.prepareStatement("INSERT INTO SHAPES (id, district_name, shape) VALUES (?, ?, ?);")
-        stmt.setInt(1, id.toInt)
-        stmt.setString(2, districtName)
-        stmt.setWKT(3, wkt)
+      if (values.next() && values.getInt(1) == 0) {
+        while (lines.hasNext) {
+          val (id, districtName, wkt) = (lines.next, lines.next, lines.next)
 
-        stmt.execute()
+          val stmt = c.prepareStatement("INSERT INTO SHAPES (id, district_name, shape) VALUES (?, ?, ?);")
+          stmt.setInt(1, id.toInt)
+          stmt.setString(2, districtName)
+          stmt.setWKT(3, wkt)
+
+          stmt.execute()
+        }
       }
     }
   }
